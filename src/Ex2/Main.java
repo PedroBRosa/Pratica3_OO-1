6053efef177aea0e2cc81ae4d16063b21d4dd97c
Pedro Rosa <92191060+PedroBRosa@users.noreply.github.com>
2022-03-30 15:17:29 -0300

    :construction:
    Realizando a chamada dos objetos no main e estruturando os métodos dentro de cada class.

--- src/Ex2/Main.java
+++ src/Ex2/Main.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int menu = 0,matricula,producao;
-        double  percentual,vendas,salario;
+        double  percentual,vendas,salario,valor;
         String nome;
         Scanner ler = new Scanner(System.in);
-
+        System.out.println("Escreva o nome do funcionario: ");
+        nome = ler.nextLine();
+        System.out.println("Escreva o salario do funcionario: ");
+        salario = ler.nextDouble();
+        System.out.println("Escreva a matricula: ");
+        matricula = ler.nextInt();
         switch (menu) {
             case 1:
                 System.out.println("SALARIO FIXO");
-                FPadrao f = new FPadrao(matricula,nome,salario);
+                FPadrao fPadrao = new FPadrao(matricula,salario,nome);
             case 2:
                 System.out.println("COMISSÃO");
+                System.out.println("Escreva o Percentual: ");
+                percentual = ler.nextDouble();
+                System.out.println("Escreva as Vendas: ");
+                vendas = ler.nextDouble();
+                FComissionado fComissionado = new FComissionado(matricula,salario,nome,percentual,vendas);
             case 3:
                 System.out.println("PRODUTIVIDADE");
+                FProdutividade fProdutividade = new FProdutividade(matricula,salario,nome,valor,producao);
             default:
                 System.out.println("Opção Invalida");
 
